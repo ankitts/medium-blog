a1dbd6c07d9939fcdcd95ac07643439e0771bf71
Ankit <ankitrsomani02@gmail.com>
2024-04-10 15:37:12 +0530

    added basic frontend structure

---
+++ client/src/App.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Blog from "./pages/Blog"
+import Blogs from "./pages/Blogs"
+import Signin from "./pages/Signin"
+import Signup from "./pages/Signup"
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='*' element={<Signin />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/signin' element={<Signin />} />
+          <Route path='/blogs' element={<Blogs />} />
+          <Route path='/blog/:id' element={<Blog />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
